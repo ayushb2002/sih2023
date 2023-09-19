11c4e01f38b359bb30dce6ee0b9497595bf05d49
Ayush Bansal <ayushb6732@gmail.com>
2023-09-19 12:41:48 +0530

    Secret key hidden

--- rescue/rescue/settings.py
+++ rescue/rescue/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-689&u4h=f2lr8c$0&4&y*%c-lwtqq_h%w6q+9+-11vpe%uz)!0"
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

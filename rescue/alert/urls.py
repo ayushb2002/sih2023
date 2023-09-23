64a5ae77309431bce755f868d50de7b508963daf
Ayush Bansal <ayushb6732@gmail.com>
2023-09-24 00:05:23 +0530

    Added alert pages

--- rescue/alert/urls.py
+++ rescue/alert/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("raiseAlert", views.raiseAlert, name="raiseAlert"),
     path("sendAlert", views.sendAlert, name="sendAlert"),
+    path("viewRaisedAlert", views.viewRaisedAlert, name="viewRaisedAlert"),
 ]
--- rescue/alert/views.py
+++ rescue/alert/views.py
@@ -16,6 +16,35 @@ def raiseAlert(request):
     
     return render(request, "alert/sendAlert.html", context)
 
+def viewRaisedAlert(request):
+    if not request.session.get('username'):
+        return HttpResponse('404! Page not found!')
+    
+    context = {}
+    context['username'] = request.session.get('username')
+    context['name'] = request.session.get('name')
+    context['type'] = request.session.get('type')
+    
+    user = User.objects.get(username=request.session.get('username'))
+    member = Member.objects.get(user=user)
+    team = RescueTeam.objects.get(id=member.team.id)
+    alerts = Alert.objects.filter(city=team.city, state=team.state).values()
+    alertList = []
+    
+    for alert in alerts:
+        alertList.append(
+            {
+                "description": alert.description,
+                "location": alert.location,
+                "city": alert.city,
+                "state": alert.state,
+                "categories": alert.categories
+            }
+        )
+    
+    context["alertList"] = alertList
+    return render(request, "alert/viewAlert.html", context)
+
 def sendAlert(request):
     if not request.session.get('username'):
         return HttpResponse('404! Page not found!')
@@ -29,25 +58,23 @@ def sendAlert(request):
         try:
             user = User.objects.get(username=request.session.get('username'))
             member = Member.objects.get(user=user)
-            team = RescueTeam.objects.get(id=member.team_id)
-            location = request.POST.get('location')
-            city = request.POST.get('city')
-            state = request.POST.get('state')
+            team = RescueTeam.objects.get(id=member.team.id)
+            location = request.POST.get('gps')
+            city = request.POST.get('city').upper()
+            state = request.POST.get('state').upper()
             description = request.POST.get('description')
-            categories = request.POST.getList('categories')
+            categories = request.POST.getlist('categories')
             categories = [i.upper() for i in categories]
             
             category_matches = Q()
             for category in team.category:
                 category_matches |= Q(category__contains=[category])
 
-            available = RescueTeam.objects.filter(
-                category_matches, answered=False, completed=False
-            ).values()
-            
+            available = RescueTeam.objects.filter(category_matches).values()
             responders = []
             for data in available:
-                responders.append(data.user)
+                respUser = User.objects.get(id=data["user_id"])
+                responders.append(respUser.username)
                 
             alert = Alert.objects.create(from_employee=user, from_team=team, location=location, city=city, state=state, categories=categories, description=description, responders=responders)
             alert.save()
